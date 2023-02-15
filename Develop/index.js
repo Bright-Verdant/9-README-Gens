0d0d655de37718587fd8c4e4890f5c04db1f730e
raymond-stack-vanasse <raymond.stackvanasse@gmail.com>
2023-02-14 22:51:17 -0600

    added js to generate and write a quality README sans license link

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,101 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Questions for user input
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: "What's the title of your project?",
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information for your project:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Provide contribution guidelines for your project:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions for your project:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What type of license does your project have?',
+      choices: ['MIT', 'GPL', 'Apache', 'BSD', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+  ])
 
-// Function call to initialize app
-init();
+
+  .then((answers) => {
+    const readmeContent = `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Questions
+If you have any questions, please feel free to contact me:
+- GitHub: https://github.com/${answers.github}
+- Email: ${answers.email}
+`;
+
+
+//  Create a function to write README file
+
+    fs.writeFile('./README.md', readmeContent, (err) => {
+      if (err) throw err;
+      console.log('README.md file has been created successfully!');
+    });
+  });
